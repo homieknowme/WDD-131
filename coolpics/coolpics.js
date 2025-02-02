8a7733f1fed65545061675c00b8a7c32518d8595
homieknowme <homieknowme@gmail.com>
2025-02-02 00:56:07 -0600

    Cool Pics Part 2

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -4,3 +4,30 @@ const menu = document.querySelector('.menu-list');
 menuButton.addEventListener('click', () => {
     menu.classList.toggle('open');
 });
+
+function viewerTemplate(pic, alt) {
+    return `<div class="viewer">
+        <button class="close-viewer">X</button>
+        <img src="${pic}" alt="${alt}">
+    </div>`;
+}
+
+function viewHandler(event) {
+
+    const clickedElement = event.target;
+
+    if (clickedElement.tagName !== "IMG") return;
+
+    const srcParts = clickedElement.src.split("-");
+    const fullImageSrc = srcParts[0] + "-full.jpeg";
+
+    document.body.insertAdjacentHTML("afterbegin", viewerTemplate(fullImageSrc, clickedElement.alt));
+
+    document.querySelector(".close-viewer").addEventListener("click", closeViewer);
+}
+
+function closeViewer() {
+    document.querySelector(".viewer").remove();
+}
+
+document.querySelector(".gallery").addEventListener("click", viewHandler);
